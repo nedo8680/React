395d9819eda229acf31b959785213f385545d786
Pedro Fuentes <pedrofuentes@MacBook-Pro-de-Pedro.local>
2020-02-05 11:12:42 -0500

    Implementación base de una aplicación con react(create-react-app)

---
+++ base/src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = ({fecha}) =>  {
+    
+    return ( 
+        <footer>Todos ls derechos reservados &copy; {fecha}</footer>
+     )};
+ 
+export default Footer;
